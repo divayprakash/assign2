7fb01d31eaa8415255874977c9fe54e3a1f9f2d1
Divay Prakash <divay@divay-Inspiron-N5050>
2015-08-19 09:42:19 +0530

    Commented code

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
- 
+	// included libraries
+
 #define DELAY_MS 300
- 
+	// global constant
+
 int main ()
 {
 	DDRB = 0xFF;
- 	// PORTB, output
+ 	// set data direction register for output on PORTB
  	while(1) 
  	{
  		PORTB = 0x20;
-  		//Pin 0, high
+  		// set Pin 0 of PORTB at high
     	_delay_ms(DELAY_MS); 
-  		//delay
+  		// delay
     	PORTB = 0x00;
-    	// Pin 0, low
+    	// ser Pin 0 of PORTB at low
   		_delay_ms(DELAY_MS);
  	 	//delay
   	}
